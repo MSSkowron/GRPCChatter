09f44b85279026f5e75ab06e78d4853bde06555b
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-21 07:46:23 +0200

    Improve goroutine management and error handling

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,20 +3,22 @@ package client
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/MSSkowron/GRPCChatter/proto/gen/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-// TODO: Synchronize goroutines
 type Client struct {
-	conn   *grpc.ClientConn
-	name   string
-	stream proto.GRPCChatter_ChatClient
-
+	conn         *grpc.ClientConn
+	name         string
+	stream       proto.GRPCChatter_ChatClient
 	receiveQueue chan Message
 	sendQueue    chan string
+	closeOnce    sync.Once
+	closeCh      chan struct{}
+	wg           sync.WaitGroup
 }
 
 type Message struct {
@@ -36,55 +38,75 @@ func NewClient(name string, serverAddress string) (*Client, error) {
 	}
 
 	client := &Client{
-		name:   name,
-		conn:   conn,
-		stream: stream,
-
+		name:         name,
+		conn:         conn,
+		stream:       stream,
 		receiveQueue: make(chan Message),
 		sendQueue:    make(chan string),
+		closeCh:      make(chan struct{}),
 	}
 
 	return client, nil
 }
 
 func (c *Client) Join() {
+	c.wg.Add(2)
 	go c.send()
 	go c.receive()
 }
 
 func (c *Client) Send(message string) {
-	c.sendQueue <- message
+	select {
+	case c.sendQueue <- message:
+	case <-c.closeCh:
+	}
 }
 
 func (c *Client) Receive() Message {
-	return <-c.receiveQueue
+	select {
+	case msg := <-c.receiveQueue:
+		return msg
+	case <-c.closeCh:
+		return Message{}
+	}
 }
 
 func (c *Client) send() {
+	defer c.wg.Done()
 	for {
-		msg := <-c.sendQueue
-		c.stream.Send(&proto.ClientMessage{
-			Name: c.name,
-			Body: msg,
-		})
+		select {
+		case msg := <-c.sendQueue:
+			if err := c.stream.Send(&proto.ClientMessage{
+				Name: c.name,
+				Body: msg,
+			}); err != nil {
+				return
+			}
+		case <-c.closeCh:
+			return
+		}
 	}
 }
 
 func (c *Client) receive() {
+	defer c.wg.Done()
 	for {
 		msg, err := c.stream.Recv()
 		if err != nil {
+			c.Close()
 			return
 		}
-
 		c.receiveQueue <- Message{
 			Sender: msg.Name,
 			Body:   msg.Body,
 		}
 	}
-
 }
 
 func (c *Client) Close() {
-	c.conn.Close()
+	c.closeOnce.Do(func() {
+		close(c.closeCh)
+		c.conn.Close()
+		c.wg.Wait()
+	})
 }
