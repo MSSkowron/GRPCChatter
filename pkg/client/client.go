dca06ba826ebe1068b3a0128562f5c0f71bfc8c4
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-21 08:12:37 +0200

    Refactor client's code adding proper comments to methods

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -10,51 +10,62 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+// Client represents a chat client.
 type Client struct {
-	conn         *grpc.ClientConn
-	name         string
-	stream       proto.GRPCChatter_ChatClient
-	receiveQueue chan Message
-	sendQueue    chan string
-	closeOnce    sync.Once
-	closeCh      chan struct{}
-	wg           sync.WaitGroup
+	name          string
+	serverAddress string
+	conn          *grpc.ClientConn
+	stream        proto.GRPCChatter_ChatClient
+	receiveQueue  chan Message
+	sendQueue     chan string
+	closeOnce     sync.Once
+	closeCh       chan struct{}
+	wg            sync.WaitGroup
 }
 
+// Message represents a chat message.
 type Message struct {
 	Sender string
 	Body   string
 }
 
+// NewClient creates a new chat client.
 func NewClient(name string, serverAddress string) (*Client, error) {
-	conn, err := grpc.Dial(serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to server %w", err)
-	}
-
-	stream, err := proto.NewGRPCChatterClient(conn).Chat(context.Background())
-	if err != nil {
-		return nil, fmt.Errorf("failed to create a stream with server: %w", err)
-	}
-
 	client := &Client{
-		name:         name,
-		conn:         conn,
-		stream:       stream,
-		receiveQueue: make(chan Message),
-		sendQueue:    make(chan string),
-		closeCh:      make(chan struct{}),
+		name:          name,
+		serverAddress: serverAddress,
+		receiveQueue:  make(chan Message),
+		sendQueue:     make(chan string),
+		closeCh:       make(chan struct{}),
 	}
 
 	return client, nil
 }
 
-func (c *Client) Join() {
+// Join connects to the server, starts receiving messages, and enables sending messages.
+func (c *Client) Join() error {
+	conn, err := grpc.Dial(c.serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("failed to connect to server: %w", err)
+	}
+	c.conn = conn
+
+	stream, err := proto.NewGRPCChatterClient(c.conn).Chat(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to create a stream with server: %w", err)
+	}
+	c.stream = stream
+
 	c.wg.Add(2)
 	go c.send()
 	go c.receive()
+
+	return nil
 }
 
+// Send sends a message to the server.
+// It blocks until the message is sent or returns immediately when the stream is closed and returns an empty message.
+// Join() should be called before the first usage.
 func (c *Client) Send(message string) {
 	select {
 	case c.sendQueue <- message:
@@ -62,6 +73,9 @@ func (c *Client) Send(message string) {
 	}
 }
 
+// Receive receives a message from the server.
+// It blocks until a message comes in and returns the incoming message or returns immediately when the stream is closed and returns an empty message.
+// Join() should be called before the first usage.
 func (c *Client) Receive() Message {
 	select {
 	case msg := <-c.receiveQueue:
@@ -80,6 +94,7 @@ func (c *Client) send() {
 				Name: c.name,
 				Body: msg,
 			}); err != nil {
+				c.Close()
 				return
 			}
 		case <-c.closeCh:
@@ -103,6 +118,7 @@ func (c *Client) receive() {
 	}
 }
 
+// Close gracefully terminates the client, closing the connection with the server and cleaning up associated resources.
 func (c *Client) Close() {
 	c.closeOnce.Do(func() {
 		close(c.closeCh)
