3b4c6608ee84c784726fafbf99fb93753c73c36d
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-22 21:23:34 +0200

    Add logger package for structured JSON logging using slog package

---
+++ pkg/logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+var logger *slog.Logger
+
+func init() {
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+}
+
+// Info logs the provided message at [InfoLevel].
+func Info(msg string) {
+	logger.Info(msg)
+}
+
+// Debug logs the provided message at [DebugLevel].
+func Debug(msg string) {
+	logger.Debug(msg)
+}
+
+// Error logs the provided message at [ErrorLevel].
+func Errorf(msg string) {
+	logger.Error(msg)
+}
