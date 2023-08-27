b98486e50d564def44b60ddc5b34860ff439df74
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-27 10:56:16 +0200

    Refactor ShortCodeService

--- internal/services/short_code_service.go
+++ internal/services/short_code_service.go
@@ -15,12 +15,14 @@ type ShortCodeServiceImpl struct {
 }
 
 // NewShortCodeService creates a new ShortCodeServiceImpl instance with the specified short code length.
-func NewShortCodeService(shortCodeLength int) *ShortCodeServiceImpl {
+func NewShortCodeService(shortCodeLength int) ShortCodeService {
 	return &ShortCodeServiceImpl{
 		shortCodeLength: shortCodeLength,
 	}
 }
 
+// GenerateShortCode generates a short code for a given room name.
+// It returns the generated short code.
 func (s *ShortCodeServiceImpl) GenerateShortCode(roomName string) string {
 	return rand.Str(s.shortCodeLength)
 }
