c49ce242e97ae36d61dcaa74e3955cf95c0e55c5
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-30 12:05:50 +0200

    Create a Database Interface with Postgresql Implementation

--- go.mod
+++ go.mod
@@ -13,10 +13,19 @@ require (
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgx/v5 v5.4.3
+	github.com/lib/pq v1.10.9
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,61 @@
 package database
+
+import (
+	"context"
+	"fmt"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Database interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	QueryRowContext(ctx context.Context, query string, args ...any) (*sql.Row, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	Close() error
+}
+
+// PostgresDatabase implements the Database interface for PostgreSQL.
+type PostgresDatabase struct {
+	db *sql.DB
+}
+
+// NewPostgresDatabase creates a new PostgresDatabase instance.
+func NewPostgresDatabase(ctx context.Context, connectionString string) (*PostgresDatabase, error) {
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create database connection: %w", err)
+	}
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to check database connection: %w", err)
+	}
+
+	return &PostgresDatabase{
+		db: db,
+	}, nil
+}
+
+// ExecContext executes a query that doesn't return rows.
+func (pdb *PostgresDatabase) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	return pdb.db.ExecContext(ctx, query, args...)
+}
+
+// QueryRowContext retrieves a single row.
+func (pdb *PostgresDatabase) QueryRowContext(ctx context.Context, query string, args ...any) (*sql.Row, error) {
+	return pdb.db.QueryRowContext(ctx, query, args...), nil
+}
+
+// QueryContext executes a query that returns multiple rows.
+func (pdb *PostgresDatabase) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	return pdb.db.QueryContext(ctx, query, args...)
+}
+
+// Close closes the database connection.
+func (pdb *PostgresDatabase) Close() error {
+	if err := pdb.db.Close(); err != nil {
+		return fmt.Errorf("failed to close database connection: %w", err)
+	}
+	return nil
+}
