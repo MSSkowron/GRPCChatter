14c0588dd95013bf5f8052271dc067b148ddffd7
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-31 11:48:49 +0200

    Comment Database methods

--- internal/database/database.go
+++ internal/database/database.go
@@ -9,10 +9,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Database is an interface that defines the methods required for interacting with a database.
 type Database interface {
+	// ExecContext executes a query that doesn't return rows.
+	// It should be used for INSERT, UPDATE, DELETE, and other non-query SQL statements.
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+
+	// QueryRowContext retrieves a single row from the database.
+	// It should be used for querying a single row result.
 	QueryRowContext(ctx context.Context, query string, args ...any) (*sql.Row, error)
+
+	// QueryContext executes a query that returns multiple rows.
+	// It should be used for querying multiple rows of data.
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+
+	// Close closes the database connection.
+	// It should be called when you're done using the database to release resources.
 	Close() error
 }
 
@@ -21,7 +33,8 @@ type PostgresDatabase struct {
 	db *sql.DB
 }
 
-// NewPostgresDatabase creates a new PostgresDatabase instance.
+// NewPostgresDatabase creates a new PostgresDatabase instance with the connection string and context.
+// It establishes a connection to the PostgreSQL database and verifies its availability.
 func NewPostgresDatabase(ctx context.Context, connectionString string) (*PostgresDatabase, error) {
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
@@ -37,22 +50,18 @@ func NewPostgresDatabase(ctx context.Context, connectionString string) (*Postgre
 	}, nil
 }
 
-// ExecContext executes a query that doesn't return rows.
 func (pdb *PostgresDatabase) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
 	return pdb.db.ExecContext(ctx, query, args...)
 }
 
-// QueryRowContext retrieves a single row.
 func (pdb *PostgresDatabase) QueryRowContext(ctx context.Context, query string, args ...any) (*sql.Row, error) {
 	return pdb.db.QueryRowContext(ctx, query, args...), nil
 }
 
-// QueryContext executes a query that returns multiple rows.
 func (pdb *PostgresDatabase) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
 	return pdb.db.QueryContext(ctx, query, args...)
 }
 
-// Close closes the database connection.
 func (pdb *PostgresDatabase) Close() error {
 	if err := pdb.db.Close(); err != nil {
 		return fmt.Errorf("failed to close database connection: %w", err)
