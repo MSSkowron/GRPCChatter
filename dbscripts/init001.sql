e9414b9aa9532a45a251e551c7b8b363e9873c43
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-09-07 19:13:37 +0200

    Fix database script 'init001' by removing redundant comma

--- dbscripts/init001.sql
+++ dbscripts/init001.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id bigint primary key generated always as identity,
     created_at timestamptz default NOW() NOT NULL,
     username varchar(255) unique NOT NULL,
-    password varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
 )
